bc2d34ed921ea959e01f7a37365b75530c9d427d
Shaun Kaasten <shaunk@gmail.com>
2021-01-28 11:49:39 -0500

    add my answers

--- src/koans/10_runtime_polymorphism.clj
+++ src/koans/10_runtime_polymorphism.clj
@@ -10,25 +10,28 @@
                    "!")))
 
 (defmulti diet (fn [x] (:eater x)))
-(defmethod diet :herbivore [a] __)
-(defmethod diet :carnivore [a] __)
-(defmethod diet :default [a] __)
+
+(defmethod diet :herbivore [a] (str (:name a) " eats veggies."))
+
+(defmethod diet :carnivore [a] (str (:name a) " eats animals."))
+
+(defmethod diet :default [a] (str "I don't know what " (:name a) " eats."))
 
 (meditations
   "Some functions can be used in different ways - with no arguments"
-  (= __ (hello))
+  (= "Hello World!" (hello))
 
   "With one argument"
-  (= __ (hello "world"))
+  (= "Hello, you silly world." (hello "world"))
 
   "Or with many arguments"
-  (= __
+  (= "Hello to this group: Peter, Paul, Mary!"
      (hello "Peter" "Paul" "Mary"))
 
   "Multimethods allow more complex dispatching"
   (= "Bambi eats veggies."
      (diet {:species "deer" :name "Bambi" :age 1 :eater :herbivore}))
-
+  
   "Animals have different names"
   (= "Thumper eats veggies."
      (diet {:species "rabbit" :name "Thumper" :age 1 :eater :herbivore}))
@@ -40,3 +43,4 @@
   "You may use a default method when no others match"
   (= "I don't know what Rich Hickey eats."
      (diet {:name "Rich Hickey"})))
+
--- src/koans/19_datatypes.clj
+++ src/koans/19_datatypes.clj
@@ -2,6 +2,7 @@
   (:require [koan-engine.core :refer :all]))
 
 (defrecord Nobel [prize])
+
 (deftype Pulitzer [prize])
 
 (defprotocol Award
@@ -18,30 +19,38 @@
 (deftype Razzie [category]
   Award
   (present [this recipient]
-    __))
+    (print (str "You're really the "
+               (.category this) ", "
+               recipient
+               "... sorry."
+               ))))
 
 (meditations
   "Holding records is meaningful only when the record is worthy of you"
-  (= __ (.prize (Nobel. "peace")))
+  (= "peace" (.prize (Nobel. "peace")))
 
   "Types are quite similar"
-  (= __ (.prize (Pulitzer. "literature")))
+  (= "literature" (.prize (Pulitzer. "literature")))
 
   "Records may be treated like maps"
-  (= __ (:prize (Nobel. "physics")))
+  (= "physics" (:prize (Nobel. "physics")))
 
   "While types may not"
-  (= __ (:prize (Pulitzer. "poetry")))
+  (= nil (:prize (Pulitzer. "poetry")))
 
   "Further study reveals why"
-  (= __
+  (= '(true false)
      (map map? [(Nobel. "chemistry")
                 (Pulitzer. "music")]))
 
   "Either sort of datatype can define methods in a protocol"
-  (= __
-     (with-out-str (present (Oscar. "Best Picture") "Evil Alien Conquerors")))
+  (= "Congratulations on your Best Picture Oscar, Evil Alien Conquerors!"
+     (with-out-str (present (Oscar. "Best Picture")
+                            "Evil Alien Conquerors")))
 
   "Surely we can implement our own by now"
   (= "You're really the Worst Picture, Final Destination 5... sorry."
-     (with-out-str (present (Razzie. "Worst Picture") "Final Destination 5"))))
+     (with-out-str (present (Razzie. "Worst Picture") "Final Destination 5")))
+
+  )
+
